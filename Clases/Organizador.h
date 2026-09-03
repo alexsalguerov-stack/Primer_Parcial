0f6a59ad051876f8fb6369469073b0b4235582f3
michellegonzalez-oss <michelle.gonzalez@lasallesaltillo.edu.mx>
2026-09-03 15:01:11 -0600

    Remove simularBatalla function from Organizador.h
    Removed the simularBatalla function declaration and its comments.

--- Clases/Organizador.h
+++ Clases/Organizador.h
@@ -7,9 +7,3 @@ using namespace std;
 
 // Tema 5: filtra un vector<Robot> por tipo (agrupar por disciplina)
 vector<Robot> filtrarPorTipo(const vector<Robot>& robots, const string& tipo);
-
-// Tema 7: dado un vector<Robot> con al menos 2 elementos,
-// selecciona dos robots distintos al azar y devuelve el indice del ganador
-// (indice dentro del vector original 'robots').
-// Usa <random> (mt19937), no rand().
-int simularBatalla(const vector<Robot>& robots);
