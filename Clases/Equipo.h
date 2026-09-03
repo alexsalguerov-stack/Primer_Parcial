94b770a18937671442e1454b938e73caa0490240
michellegonzalez-oss <michelle.gonzalez@lasallesaltillo.edu.mx>
2026-09-03 14:37:43 -0600

    Refactor Equipo class to use std namespace

--- Clases/Equipo.h
+++ Clases/Equipo.h
@@ -4,14 +4,16 @@
 #include "Integrante.h"
 #include "Robot.h"
 
+using namespace std;
+
 // Clase Equipo: HAS-A Integrante(s), HAS-A Robot(s).
 // Composicion practicada directamente porque es una pieza que
 // casi con certeza los equipos van a necesitar en su proyecto.
 class Equipo {
 private:
-    std::string nombreEquipo;
-    std::vector<Integrante> integrantes;
-    std::vector<Robot> robots;
+    string nombreEquipo;
+    vector<Integrante> integrantes;
+    vector<Robot> robots;
 
 public:
     explicit Equipo(const std::string& nombreEquipo);
@@ -19,9 +21,9 @@ public:
     void agregarIntegrante(const Integrante& i);
     void agregarRobot(const Robot& r);
 
-    std::string getNombreEquipo() const;
-    const std::vector<Integrante>& getIntegrantes() const;
-    const std::vector<Robot>& getRobots() const;
+    string getNombreEquipo() const;
+    const vector<Integrante>& getIntegrantes() const;
+    const vector<Robot>& getRobots() const;
 
     // Imprime un resumen legible del equipo (nombre, integrantes, robots)
     void mostrarResumen() const;
