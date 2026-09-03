255f02daa21d42780d588de9251fd1f287eb55a7
michellegonzalez-oss <michelle.gonzalez@lasallesaltillo.edu.mx>
2026-09-03 14:43:29 -0600

    Fix formatting and update comments in Organizador.h

--- Clases/Organizador.h
+++ Clases/Organizador.h
@@ -6,10 +6,10 @@
 using namespace std;
 
 // Tema 5: filtra un vector<Robot> por tipo (agrupar por disciplina)
-vector<Robot> filtrarPorTipo(const std::vector<Robot>& robots, const std::string& tipo);
+vector<Robot> filtrarPorTipo(const vector<Robot>& robots, const string& tipo);
 
 // Tema 7: dado un vector<Robot> con al menos 2 elementos,
 // selecciona dos robots distintos al azar y devuelve el indice del ganador
 // (indice dentro del vector original 'robots').
 // Usa <random> (mt19937), no rand().
-int simularBatalla(const std::vector<Robot>& robots);
+int simularBatalla(const vector<Robot>& robots);
