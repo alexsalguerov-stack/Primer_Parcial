012c6e47c0f428a7b1d9f628b69dd5dcbd5aee97
alexsalguerov-stack <alex.salguerov@gmail.com>
2026-09-03 15:05:43 -0600

    Avances del mainframe

--- mainframe.cpp
+++ mainframe.cpp
@@ -6,3 +6,24 @@
 #include <Referee.h>
 #include <Integrantes.h>
 
+Equipo registro(){
+    std::string nombre_equipo;
+    std::cout << "Ingresar nombre del equipo: ";
+    std::getline(std::cin, nombre_equipo);
+    
+    Equipo equipo(nombre_equipo);
+
+    int no_robot = 0;
+    std::cout << "¿Cuantos robots tiene el equipo? ";
+    std::cin no_robot;
+    syd::cin.ignore(); // Limpiar el buffer de entrada
+
+    for (int i = 0; i < no_robot; ++i) {
+        std::string nombre_robot, clase_robot;
+        std::cout << "  Robot " << (i + 1) << " - nombre: ";
+        std::getline(std::cin, nombre);
+        std::cout << "  Robot " << (i + 1) << " - tipo (Sumo/Seguidor de linea/Combate/...): ";
+        std::getline(std::cin, tipo);
+        equipo.agregarRobot(Robot(nombre, tipo));
+    }
+}
