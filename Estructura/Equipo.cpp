a5807710e1295b0dedb7f6a855690f5423db7a52
michellegonzalez-oss <michelle.gonzalez@lasallesaltillo.edu.mx>
2026-09-04 14:34:43 -0600

    Implement Equipo class with member functions

--- Estructura/Equipo.cpp
+++ Estructura/Equipo.cpp
@@ -1,1 +1,39 @@
+#include "Equipo.h"
+#include <iostream>
 
+Equipo::Equipo(const std::string& nombreEquipo)
+    : nombreEquipo(nombreEquipo) {}
+
+void Equipo::agregarIntegrante(const Integrante& i) {
+    integrantes.push_back(i);
+}
+
+void Equipo::agregarRobot(const Robot& r) {
+    robots.push_back(r);
+}
+
+std::string Equipo::getNombreEquipo() const {
+    return nombreEquipo;
+}
+
+const std::vector<Integrante>& Equipo::getIntegrantes() const {
+    return integrantes;
+}
+
+const std::vector<Robot>& Equipo::getRobots() const {
+    return robots;
+}
+
+void Equipo::mostrarResumen() const {
+    std::cout << "\n=== Equipo: " << nombreEquipo << " ===\n";
+
+    std::cout << "Integrantes (" << integrantes.size() << "):\n";
+    for (const auto& i : integrantes) {
+        std::cout << "  - " << i.getNombre() << " (" << i.getRol() << ")\n";
+    }
+
+    std::cout << "Robots (" << robots.size() << "):\n";
+    for (const auto& r : robots) {
+        std::cout << "  - " << r.getNombre() << " [" << r.getTipo() << "]\n";
+    }
+}
