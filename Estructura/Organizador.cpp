f0636a8db9150899d37b45578877e24894761513
michellegonzalez-oss <michelle.gonzalez@lasallesaltillo.edu.mx>
2026-09-10 13:54:04 -0600

    Remove simularBatalla function from Organizador.cpp
    Removed the simularBatalla function and its random number generation logic.

--- Estructura/Organizador.cpp
+++ Estructura/Organizador.cpp
@@ -1,5 +1,4 @@
 #include "UtilRobots.h"
-#include <random>
 #include <stdexcept>
 
 using namespace std;
@@ -12,24 +11,4 @@ vector<Robot> filtrarPorTipo(const vector<Robot>& robots, const string& tipo) {
         }
     }
     return resultado;
-}
-
-int simularBatalla(const vector<Robot>& robots) {
-    if (robots.size() < 2) {
-        throw invalid_argument("Se necesitan al menos 2 robots para simular una batalla.");
-    }
-
-    // Generador moderno de numeros aleatorios (mejor distribucion que rand())
-    static random_device rd;
-    static mt19937 gen(rd());
-
-    uniform_int_distribution<size_t> distIndice(0, robots.size() - 1);
-
-    // Elegir dos indices distintos
-    size_t idx1 = distIndice(gen);
-    size_t idx2 = distIndice(gen);
-    while (idx2 == idx1) {
-        idx2 = distIndice(gen);
-    }
-
 }
