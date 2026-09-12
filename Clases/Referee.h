a7cbdba8f3012b34552e5dd3866258be9a986af3
michellegonzalez-oss <michelle.gonzalez@lasallesaltillo.edu.mx>
2026-09-12 00:08:55 -0600

    Refactor Referee.h to use std namespace explicitly

--- Clases/Referee.h
+++ Clases/Referee.h
@@ -3,11 +3,9 @@
 #include <string>
 #include "Robot.h"
 
-using namespace std;
-
 // Tema 7: dado un vector<Robot> con al menos 2 elementos,
 // selecciona dos robots distintos al azar y devuelve el indice del ganador
 // (indice dentro del vector original 'robots').
 // Usa <random> (mt19937), no rand().
-int simularBatalla(const vector<Robot>& robots);
+int simularBatalla(const std::vector<Robot>& robots);
 
