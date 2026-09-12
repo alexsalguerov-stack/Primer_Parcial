60e5469ea5c01c4a9897af9e154b512e72d4218a
michellegonzalez-oss <michelle.gonzalez@lasallesaltillo.edu.mx>
2026-09-12 00:08:17 -0600

    Refactor Robot class to use std namespace

--- Clases/Robot.h
+++ Clases/Robot.h
@@ -1,24 +1,22 @@
 #pragma once
 #include <string>
 
-using namespace std;
-
 // Clase Robot: representa un robot participante en la competencia.
 // Atributos privados + acceso controlado (encapsulamiento).
 class Robot {
 private:
-    string nombre;
-    string tipo; // Ej: "Sumo", "Seguidor de linea", "Combate"
+    std::string nombre;
+    std::string tipo; // Ej: "Sumo", "Seguidor de linea", "Combate"
 
 public:
     // Constructor con lista de inicializacion
-    Robot(const string& nombre, const string& tipo);
+    Robot(const std::string& nombre, const std::string& tipo);
 
     // Getters
-    string getNombre() const;
-    string getTipo() const;
+    std::string getNombre() const;
+    std::string getTipo() const;
 
     // Setters (con validacion basica)
-    void setNombre(const string& nuevoNombre);
-    void setTipo(const string& nuevoTipo);
+    void setNombre(const std::string& nuevoNombre);
+    void setTipo(const std::string& nuevoTipo);
 };
